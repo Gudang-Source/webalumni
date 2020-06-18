d50950fdfef59c0d56ca1674cee60dc41d181fe0
adhywiranto44 <adhywiranto68@gmail.com>
2020-06-18 15:25:55 +0700

    benerin, tadi ngaco

--- application/views/koordinator/home.php
+++ application/views/koordinator/home.php
@@ -1,18 +1,19 @@
 <!-- START BREADCRUMB -->
 <ul class="breadcrumb">
-    <li class="active"><a href="<?= base_url('koordinator'); ?>">Beranda</a></li>
+    <li><a href="#">Home</a></li>
+    <li class="active">Beranda</li>
 </ul>
-<!-- END BREADCRUMB -->                       
+<!-- END BREADCRUMB -->
 
 <!-- PAGE CONTENT WRAPPER -->
 <div class="page-content-wrap">
-                    
-                    <!-- START WIDGETS -->                    
-                    <div class="row">
-                        <div class="col-md-3">
-                            
-                            <!-- START WIDGET SLIDER -->
-                            <div class="widget widget-default widget-carousel">
+
+    <!-- START WIDGETS -->
+    <div class="row">
+        <div class="col-md-3">
+
+            <!-- START WIDGET SLIDER -->
+            <!-- <div class="widget widget-default widget-carousel">
                                 <div class="owl-carousel" id="owl-example">
                                     <div>                                    
                                         <div class="widget-title">Total Visitors</div>                                                                        
@@ -33,14 +34,14 @@
                                 <div class="widget-controls">                                
                                     <a href="#" class="widget-control-right widget-remove" data-toggle="tooltip" data-placement="top" title="Remove Widget"><span class="fa fa-times"></span></a>
                                 </div>                             
-                            </div>         
-                            <!-- END WIDGET SLIDER -->
-                            
-                        </div>
-                        <div class="col-md-3">
-                            
-                            <!-- START WIDGET MESSAGES -->
-                            <div class="widget widget-default widget-item-icon" onclick="location.href='pages-messages.html';">
+                            </div>          -->
+            <!-- END WIDGET SLIDER -->
+
+        </div>
+        <div class="col-md-3">
+
+            <!-- START WIDGET MESSAGES -->
+            <!-- <div class="widget widget-default widget-item-icon" onclick="location.href='pages-messages.html';">
                                 <div class="widget-item-left">
                                     <span class="fa fa-envelope"></span>
                                 </div>                             
@@ -52,14 +53,14 @@
                                 <div class="widget-controls">                                
                                     <a href="#" class="widget-control-right widget-remove" data-toggle="tooltip" data-placement="top" title="Remove Widget"><span class="fa fa-times"></span></a>
                                 </div>
-                            </div>                            
-                            <!-- END WIDGET MESSAGES -->
-                            
-                        </div>
-                        <div class="col-md-3">
-                            
-                            <!-- START WIDGET REGISTRED -->
-                            <div class="widget widget-default widget-item-icon" onclick="location.href='pages-address-book.html';">
+                            </div>                             -->
+            <!-- END WIDGET MESSAGES -->
+
+        </div>
+        <div class="col-md-3">
+
+            <!-- START WIDGET REGISTRED -->
+            <!-- <div class="widget widget-default widget-item-icon" onclick="location.href='pages-address-book.html';">
                                 <div class="widget-item-left">
                                     <span class="fa fa-user"></span>
                                 </div>
@@ -71,14 +72,14 @@
                                 <div class="widget-controls">                                
                                     <a href="#" class="widget-control-right widget-remove" data-toggle="tooltip" data-placement="top" title="Remove Widget"><span class="fa fa-times"></span></a>
                                 </div>                            
-                            </div>                            
-                            <!-- END WIDGET REGISTRED -->
-                            
-                        </div>
-                        <div class="col-md-3">
-                            
-                            <!-- START WIDGET CLOCK -->
-                            <div class="widget widget-danger widget-padding-sm">
+                            </div>                             -->
+            <!-- END WIDGET REGISTRED -->
+
+        </div>
+        <div class="col-md-3">
+
+            <!-- START WIDGET CLOCK -->
+            <!-- <div class="widget widget-danger widget-padding-sm">
                                 <div class="widget-big-int plugin-clock">00:00</div>                            
                                 <div class="widget-subtitle plugin-date">Loading...</div>
                                 <div class="widget-controls">                                
@@ -95,20 +96,20 @@
                                         <a href="#"><span class="fa fa-calendar"></span></a>
                                     </div>
                                 </div>                            
-                            </div>                        
-                            <!-- END WIDGET CLOCK -->
-                            
-                        </div>
-                    </div>
-                    <!-- END WIDGETS -->                    
-                    
-                  
-                    
-                  <div class="row">
-                        <div class="col-md-4">
-                            
-                            <!-- START SALES & EVENTS BLOCK -->
-                            <div class="panel panel-default">
+                            </div>                         -->
+            <!-- END WIDGET CLOCK -->
+
+        </div>
+    </div>
+    <!-- END WIDGETS -->
+
+
+
+    <div class="row">
+        <div class="col-md-4">
+
+            <!-- START SALES & EVENTS BLOCK -->
+            <!-- <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <div class="panel-title-box">
                                         <h3>Sales & Event</h3>
@@ -129,14 +130,14 @@
                                 <div class="panel-body padding-0">
                                     <div class="chart-holder" id="dashboard-line-1" style="height: 200px;"></div>
                                 </div>
-                            </div>
-                            <!-- END SALES & EVENTS BLOCK -->
-                            
-                        </div>
-                        <div class="col-md-4">
-                            
-                            <!-- START USERS ACTIVITY BLOCK -->
-                            <div class="panel panel-default">
+                            </div> -->
+            <!-- END SALES & EVENTS BLOCK -->
+
+        </div>
+        <div class="col-md-4">
+
+            <!-- START USERS ACTIVITY BLOCK -->
+            <!-- <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <div class="panel-title-box">
                                         <h3>Users Activity</h3>
@@ -157,14 +158,14 @@
                                 <div class="panel-body padding-0">
                                     <div class="chart-holder" id="dashboard-bar-1" style="height: 200px;"></div>
                                 </div>                                    
-                            </div>
-                            <!-- END USERS ACTIVITY BLOCK -->
-                            
-                        </div>
-                        <div class="col-md-4">
-                            
-                            <!-- START VISITORS BLOCK -->
-                            <div class="panel panel-default">
+                            </div> -->
+            <!-- END USERS ACTIVITY BLOCK -->
+
+        </div>
+        <div class="col-md-4">
+
+            <!-- START VISITORS BLOCK -->
+            <!-- <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <div class="panel-title-box">
                                         <h3>Visitors</h3>
@@ -185,20 +186,20 @@
                                 <div class="panel-body padding-0">
                                     <div class="chart-holder" id="dashboard-donut-1" style="height: 200px;"></div>
                                 </div>
-                            </div>
-                            <!-- END VISITORS BLOCK -->
-                            
-                        </div>
-                    </div>
+                            </div> -->
+            <!-- END VISITORS BLOCK -->
 
-                    <!-- START DASHBOARD CHART -->
-                    <div class="block-full-width">
+        </div>
+    </div>
+
+    <!-- START DASHBOARD CHART -->
+    <!-- <div class="block-full-width">
                         <div id="dashboard-chart" style="height: 250px; width: 100%; float: left;"></div>
                         <div class="chart-legend">
                             <div id="dashboard-legend"></div>
                         </div>                                                
-                    </div>                    
-                    <!-- END DASHBOARD CHART -->
-                  
-                </div>
-                <!-- END PAGE CONTENT WRAPPER -->    
+                    </div>                     -->
+    <!-- END DASHBOARD CHART -->
+
+</div>
+<!-- END PAGE CONTENT WRAPPER -->
