35302df8cde9ac466dee85a30137e3e4dc72e7c1
dicky Ardianto <dikiardianto19@gmail.com>
2020-06-23 14:46:32 +0700

    Menambahkan fitur dashboar pada menu (admin)

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -14,14 +14,17 @@ if (defined('BASEPATH') or exit('No direct script access allowed'));
  * 2020
 */
 
-class Home extends MY_Controller 
+class Home extends MY_Controller
 {
     // ==================================================
     // ------------------ CONTSTRUCTOR ------------------
     // ==================================================    
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
         $this->load->model('AdminHomeModel');
+        $this->load->model('M_berita');
+
 
         if ($this->session->userdata('logged_in') == '' && $this->session->userdata('username') == '' && $this->session->userdata('role') == '') {
             redirect('login');
@@ -40,10 +43,20 @@ class Home extends MY_Controller
     // ==================================================
     // ---------------------- READ ----------------------
     // ==================================================
-    function index() {
+    function index()
+    {
         $data['title'] = 'Beranda Admin';
         $data['info'] = $this->AdminHomeModel->getInfoBySessionId();
-        
+        $data['jumlah_anggota'] = $this->AdminHomeModel->hitungJumlahAnggota();
+        $data['jumlah_anggota_belum_verifikasi'] = $this->AdminHomeModel->hitungJumlahAnggotaBelumVerifikasi();
+        $data['jumlah_pendaftar_anggota'] = $this->AdminHomeModel->pendaftarAnggota();
+        $data['jumlah_pendaftar_alumni'] = $this->AdminHomeModel->pendaftarAlumni();
+        $data['berita'] = $this->AdminHomeModel->beritaTerkini();
+        $data['jumlahBeritaNon'] = $this->AdminHomeModel->hitungJumlahBeritaNon();
+        $data['komunitas'] = $this->AdminHomeModel->komunitasTerkini();
+        $data['jumlahKomunitasNon'] = $this->AdminHomeModel->hitungJumlahKomunitasNon();
+        $data['jumlahForbisNon'] = $this->AdminHomeModel->hitungJumlahForbisNon();
+
         if ($this->session->userdata('role') == 1) {
             $this->admin_render('admin/home', $data);
         }
--- application/models/AdminHomeModel.php
+++ application/models/AdminHomeModel.php
@@ -8,12 +8,75 @@ defined('BASEPATH') or exit('No direct script access allowed');
  */
 class AdminHomeModel extends CI_Model
 {
-	
+
 	function getInfoBySessionId()
 	{
 		$this->db->select('nama_lengkap, nama_foto');
 		$this->db->where('user_id', $this->session->userdata('uid'));
 
 		return $this->db->get('tb_anggota')->result();
+	}
+
+	public function hitungJumlahAnggota()
+	{
+		$query = $this->db->query("SELECT * FROM tb_anggota WHERE status_anggota = 1");
+		$total = $query->num_rows();
+		return $total;
+	}
+
+	public function hitungJumlahAnggotaBelumVerifikasi()
+	{
+		$query = $this->db->query("SELECT * FROM tb_anggota");
+		$total = $query->num_rows();
+		return $total;
+	}
+
+	public function pendaftarAnggota()
+	{
+		$query = $this->db->query("SELECT * FROM `tb_user` WHERE `role` = 3");
+		$total = $query->num_rows();
+		return $total;
+	}
+
+	public function pendaftarAlumni()
+	{
+		$query = $this->db->query("SELECT * FROM `tb_user` WHERE `role` = 4");
+		$total = $query->num_rows();
+		return $total;
+	}
+
+	public function beritaTerkini()
+	{
+		$date = date('Y-m-d');
+		$query = "SELECT * FROM `tb_berita` WHERE `date_created` LIKE '$date' AND stat_berita = 1";
+		return $this->db->query($query)->result();
+	}
+
+	public function hitungJumlahBeritaNon()
+	{
+		$query = $this->db->query("SELECT * FROM tb_berita WHERE stat_berita = 0");
+		$total = $query->num_rows();
+		return $total;
+	}
+
+	public function komunitasTerkini()
+	{
+		$date = date('Y-m-d');
+		$query = "SELECT * FROM `tb_komunitas` WHERE `date_created` LIKE '$date' AND stat_komunitas = 1";
+		return $this->db->query($query)->result();
+	}
+
+	public function hitungJumlahKomunitasNon()
+	{
+		$query = $this->db->query("SELECT * FROM tb_komunitas WHERE stat_komunitas = 0");
+		$total = $query->num_rows();
+		return $total;
+	}
+
+	public function hitungJumlahForbisNon()
+	{
+		$query = $this->db->query("SELECT * FROM tb_forum_bisnis WHERE stat_forbis = 0");
+		$total = $query->num_rows();
+		return $total;
 	}
 }
