280b0b2fe6889dc4e55f554894941824b1456cc8
adhywiranto44 <adhywiranto68@gmail.com>
2020-06-24 19:20:12 +0700

    memperbaiki navbar berita, forbis, dan komunitas agar sesuai dengan commit sebelumnya

--- application/controllers/Berita.php
+++ application/controllers/Berita.php
@@ -9,9 +9,17 @@ if (defined('BASEPATH') or exit('No direct script access allowed'));
 
 class Berita extends MY_Controller
 {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('FrontPageModel');
+        $this->load->model('M_berita');
+    }
+
     function index()
     {
         $data['title'] = 'Berita';
+        $data['info'] = $this->FrontPageModel->getInfoBySessionId();
         $this->frontend_render('frontend/berita/index', $data);
     }
 }
