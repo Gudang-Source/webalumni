5e11c2e4a653f580d6fa42d66a55c1f1be5f1e28
dicky Ardianto <dikiardianto19@gmail.com>
2020-06-25 10:55:16 +0700

    mengubah copyright menjadi tahun 2020

--- application/views/login/Login.php
+++ application/views/login/Login.php
@@ -1,26 +1,30 @@
         <div class="login-container lightmode">
-        
+
             <div class="login-box animated fadeInDown">
                 <div class="login-logo"></div>
                 <div class="login-body">
                     <strong><?= showFlashMessage(); ?></strong>
-                    
+
                     <div class="login-title"><strong>Login</strong> dengan menggunakan akun Anda</div>
-                    <form action="<?= base_url('auth'); ?>" class="form-horizontal" method="post" id="form-login-validate">
+                    <form action="<?= base_url('auth'); ?>" class="form-horizontal" method="post"
+                        id="form-login-validate">
                         <div class="form-group">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" placeholder="Username" name="userName" maxlength="50" required />
+                                <input type="text" class="form-control" placeholder="Username" name="userName"
+                                    maxlength="50" required />
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-md-12">
-                                <input type="password" class="form-control" placeholder="Password" name="passWord" maxlength="50" required />
+                                <input type="password" class="form-control" placeholder="Password" name="passWord"
+                                    maxlength="50" required />
                             </div>
                         </div>
                         <div class="form-group">
 
                             <div class="col-md-6">
-                                <a href="<?= base_url('login/ForgotPassword'); ?>" class="btn btn-link btn-block">Forgot your password?</a>
+                                <a href="<?= base_url('login/ForgotPassword'); ?>" class="btn btn-link btn-block">Forgot
+                                    your password?</a>
                             </div>
 
                             <div class="col-md-6 pull-right">
@@ -28,13 +32,14 @@
                             </div>
                         </div>
                         <div class="login-subtitle">
-                            Belum jadi anggota IKASMA3BDG? Silahkan daftar <a href="<?= base_url('register'); ?>">di sini</a>
+                            Belum jadi anggota IKASMA3BDG? Silahkan daftar <a href="<?= base_url('register'); ?>">di
+                                sini</a>
                         </div>
                     </form>
                 </div>
                 <div class="login-footer">
                     <div class="pull-left">
-                        &copy; 2018 <a href="<?= base_url(); ?>" title="IKASMA3BDG">IKASMA3BDG</a>
+                        &copy; 2020 <a href="<?= base_url(); ?>" title="IKASMA3BDG">IKASMA3BDG</a>
                     </div>
                     <div class="pull-right">
                         <a href="<?= base_url('#about'); ?>">About</a> |
@@ -43,9 +48,9 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
 
         <script type="text/javascript">
-            $("#form-login-validate").validate();
+$("#form-login-validate").validate();
         </script>
